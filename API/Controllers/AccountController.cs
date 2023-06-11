d3ee7cdadb9c874a0f05ba754cc44d18a4a5683a
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2023-06-11 12:11:57 +0200

    Add RegisterDto object to register method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -16,15 +18,17 @@ namespace API.Controllers
         }
         
         [HttpPost("register")] // POST: api/account/register
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
-            // przez użycie 'using', jeśli skończymy z tą klasą wywoła się //metoda Dispose, która usunię ją (klasę) z pamięci
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+            // przez użycie 'using', jeśli skończymy z tą klasą wywoła się metoda Dispose, która usunię ją (klasę) z pamięci
             using var hmac = new HMACSHA512(); 
 
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                // w c# i ts kod jest czuły na znaki, więc najlepiej otrzymaną wartość od klienta przekonwertować do lowercase
+                UserName = registerDto.Username.ToLower(), 
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -32,6 +36,11 @@ namespace API.Controllers
             await _context.SaveChangesAsync();
 
             return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
