b95df6c1a97e0ef93c3a5c6173e37e09e402e9b6
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2023-06-12 11:49:18 +0200

    Inject TokenService into AccountController.cs

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using API.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,15 +13,18 @@ namespace API.Controllers
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
-        public AccountController(DataContext context)
+        private readonly ITokenService _tokenService;
+        public AccountController(DataContext context,
+        ITokenService tokenService)
         {
+            _tokenService = tokenService;
             _context = context;
             
         }
         
         [HttpPost("register")] // POST: api/account/register
         //nie muszę dodawać atrybutu [From Body przed RegisterDto], ponieważ dziedziczę z BaseApiController
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto) 
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto) 
         {
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
             // przez użycie 'using', jeśli skończymy z tą klasą wywoła się metoda Dispose, która usunię ją (klasę) z pamięci
@@ -36,11 +41,15 @@ namespace API.Controllers
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users.SingleOrDefaultAsync(x => 
             x.UserName == loginDto.Username.ToLower());
@@ -56,7 +65,11 @@ namespace API.Controllers
                 if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
             }
 
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         private async Task<bool> UserExists(string username)
