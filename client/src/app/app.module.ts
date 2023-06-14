43e73779e6324240d87a0a4fba6d9fae4ceee5ed
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2023-06-14 15:51:10 +0200

    Generate member-detail component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { MemberListComponent } from './members/member-list/member-list.component
     NavComponent,
     HomeComponent,
     RegisterComponent
+    RegisterComponent,
     MemberListComponent,
+    MemberDetailComponent,
   ],
   imports: [
     BrowserAnimationsModule,
