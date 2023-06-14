2953e1fbec1fa91a3ba3286d0b917af61a389962
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2023-06-14 15:51:40 +0200

    Generate messages component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,17 +13,18 @@ import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
     RegisterComponent,
     MemberListComponent,
     MemberDetailComponent,
     ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserAnimationsModule,
