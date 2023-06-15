0c6e878b1b510219634399079fec1df70705c7a1
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2023-06-15 16:47:02 +0200

    Add methods testing errors on client side

--- client/src/app/errors/test-error/test-error.component.ts
+++ client/src/app/errors/test-error/test-error.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-error.component.css']
 })
 export class TestErrorComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'buggy/not-found').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/bad-request').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/server-error').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+  get401Error() {
+    this.http.get(this.baseUrl + 'buggy/auth').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+  get400ValidationError() {
+    this.http.get(this.baseUrl + 'account/register').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
   }
 
 }
