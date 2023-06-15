97ee08c60b15052016848b66571a119068c3fb0e
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2023-06-15 15:50:03 +0200

    Create BuggyController for errors testing

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,7 +7,8 @@ namespace API.DTOs
         [Required]
         public string Username { get; set; }  
         
-        [Required]  
+        [Required]
+        [StringLength(8, MinimumLength = 4)]  
         public string Password { get; set; }
         
     }
