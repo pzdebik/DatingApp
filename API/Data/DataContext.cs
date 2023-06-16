efe7f2552bb7c3d2bc294bbd7240b22561a64cd6
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2023-06-16 12:31:19 +0200

    Add comments to DataContext.cs

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,6 +10,10 @@ namespace API.Data
         {
         }
 
+        // nie muszę tworzyć DbSeta dla encji Photo, dlatego, że:
+        // - nie chcę mieć zdjęć, które nie są przypisane do usera
+        // - nie chcę, aby jeden user mógł dodawać zdj drugiemu userowi
+        // - nie chcę móc wysyłać zapytań do bazy, aby pobrać jakieś zdjęcie dla randomowego usera
         public DbSet<AppUser> Users { get; set; }
     }
 }
