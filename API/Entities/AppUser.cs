52a5a2f803064b624c7d120c554275c5b7feed77
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2023-06-16 12:33:25 +0200

    Add again PasswordSalt property to AppUser.cs

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,6 +7,7 @@ namespace API.Entities
         public int Id { get; set; }
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
         public DateOnly DateOfBirth {get; set;}
         public string KnownAs {get; set;}
         public DateTime Created {get; set;} = DateTime.UtcNow;
@@ -17,7 +18,7 @@ namespace API.Entities
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        
+
         public List<Photo> Photos {get; set;} = new();
 
 
