fdd3e84be0e3d6e497a1a15e7e0a9a380c0d3771
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2023-06-17 11:32:39 +0200

    Subscribe to currentUser$ and add auth header to each user

--- client/src/app/_interceptors/jwt.interceptor.ts
+++ client/src/app/_interceptors/jwt.interceptor.ts
@@ -5,14 +5,29 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
+import { AccountService } from '../_services/account.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private accountService: AccountService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({ // pobiera dane tylko raz i następuje odsubskrybowanie 
+      // dla każdej otrzymanej wartości user, dodaje nagłówek autoryzacyjny 
+      //do żądania HTTP zawierającego token użytkownika.
+      next: user => {
+        if (user) {
+          request = request.clone({
+            setHeaders: {
+              Authorization: `Bearer ${user.token}`
+            }
+          })
+        }
+      }
+    })
+
     return next.handle(request);
   }
 }
