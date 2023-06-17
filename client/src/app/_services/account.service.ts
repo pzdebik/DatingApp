a091600cee614bf4d43db1d79e09f75140c1c7e5
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2023-06-17 09:19:09 +0200

    Put environment variable ino baseUrl

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,19 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { User } from '../_models/user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new BehaviorSubject<User | null>(null); // BehaviorSubject - przechowuje info o bieżącym userze
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
   login(model: any){
-    return this.http.post<User>(this.baseUrl + '/account/login', model).pipe(
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
         const user = response;
         if (user) {
@@ -27,7 +28,7 @@ export class AccountService {
 
   register(model: any) {
     // przekazuję model
-    return this.http.post<User>(this.baseUrl + '/account/register', model).pipe(
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
       map(user => {
         if (user) {
           localStorage.setItem('user', JSON.stringify(user));
