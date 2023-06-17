48e65f05ca4f63a5434167206dd9f7cb677d3562
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2023-06-17 09:41:43 +0200

    Delete HttpClient from HomeComponent

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,7 +9,7 @@ export class HomeComponent implements OnInit {
   registerMode = false;
   users: any;
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
