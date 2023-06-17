5a4d0e56b7036d9c339cd5f414a8f8fb51011b03
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2023-06-17 09:42:24 +0200

    Add loadMembers method to MemberListComponent

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-list',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
+  members: Member[] = []; // właściwość przechowująca wszystkich członków aplikacji
 
-  constructor() { }
+  constructor(private memberService: MembersService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { // metoda cyklu życia komponentu
+    this.loadMembers(); // lista członków zostaje załadowana podczas inicjalizacji komponentu
+  }
+
+  // Wywołuje usługę memberService i subskrybuje się do wyniku, który otrzymuje w postaci strumienia (observable).
+  loadMembers() { 
+    this.memberService.getMembers().subscribe({
+      // co ma się stać po otrzymaniu danych -> przypisuje ona otrzymaną listę członków (members) do właściwości this.members komponentu.
+      next: members => this.members = members 
+    })
   }
 
 }
