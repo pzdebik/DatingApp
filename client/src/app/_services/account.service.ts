1dbf41dd4d743db6d5670b72d8da3736f3f5ea4d
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2023-06-23 14:22:59 +0200

    Add comments to Account Service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,20 +1,31 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http'; // umożliwia wykonywanie żądań HTTP w aplikacji Angular.
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs'; // używane do zarządzania strumieniem danych.
 import { User } from '../_models/user';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment'; // zawiera zmienne środowiskowe, takie jak adres URL do API.
 
+// znacza, że ta usługa jest dostępna na poziomie całej aplikacji (jako usługa globalna) 
+// i zostanie dostarczona przez Angular na żądanie. 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' 
 })
+
 export class AccountService {
   baseUrl = environment.apiUrl;
-  private currentUserSource = new BehaviorSubject<User | null>(null); // BehaviorSubject - przechowuje info o bieżącym userze
+  // BehaviorSubject - przechowuje info o bieżącym userze. Początkowo jest ustawiony na null.
+  private currentUserSource = new BehaviorSubject<User | null>(null); 
+  // currentUser$ jest strumieniem (observable), który można subskrybować, aby uzyskać dostęp 
+  //do bieżącego użytkownika. Jest to publiczne pole i można je subskrybować z innych komponentów.
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
-  login(model: any){
+  // Odpowiedź z serwera jest przetwarzana przez operator map, który pobiera response i 
+  //przypisuje go do zmiennej user. Jeśli user istnieje, jego dane są zapisywane w 
+  //pamięci lokalnej przeglądarki za pomocą localStorage.setItem, a następnie aktualizowane 
+  //są dane w obiekcie currentUserSource za pomocą this.currentUserSource.next(user). 
+  //Na końcu metoda zwraca strumień (Observable) z typem User.
+  login(model: any){ //  Przyjmuje model jako argument, który reprezentuje dane logowania
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
         const user = response;
@@ -38,11 +49,11 @@ export class AccountService {
     ) 
   }
 
-  setCurrentUser(user: User) {
+  setCurrentUser(user: User) { // służy do ustawienia bieżącego użytkownika
     this.currentUserSource.next(user);
   }
 
-  logout() {
+  logout() { //  Usuwa dane użytkownika z pamięci lokalnej przeglądarki
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
   }
